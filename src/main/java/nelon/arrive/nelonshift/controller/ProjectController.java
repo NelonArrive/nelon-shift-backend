101f003b744a4ab252a7e8a6c60578e180da5d3e
NelonArrive <nelon.arrive@gmail.com>
2025-11-06 11:19:27 +0500

    Added interfaces ProjectController and ShiftController file

--- src/main/java/nelon/arrive/nelonshift/controller/ProjectController.java
+++ src/main/java/nelon/arrive/nelonshift/controller/ProjectController.java
@@ -1,10 +1,17 @@
 package nelon.arrive.nelonshift.controller;
 
 import lombok.RequiredArgsConstructor;
+import nelon.arrive.nelonshift.dto.PageResponse;
+import nelon.arrive.nelonshift.dto.ProjectDTO;
 import nelon.arrive.nelonshift.entity.Project;
+import nelon.arrive.nelonshift.entity.Project.ProjectStatus;
 import nelon.arrive.nelonshift.service.ProjectService;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
 
 @RestController
 @RequestMapping("${api.prefix}/projects")
@@ -12,13 +19,8 @@ import org.springframework.web.bind.annotation.*;
 public class ProjectController {
 	private final ProjectService projectService;
 	
-	@PostMapping
-	public Project create(@RequestBody Project project) {
-		return projectService.create(project);
-	}
-	
 	@GetMapping
-	public ResponseEntity<PageResponse<ProjectDTO>> getAll(
+	public ResponseEntity<PageResponse<ProjectDTO>> getProjects(
 		@RequestParam(required = false) String name,
 		@RequestParam(required = false) ProjectStatus status,
 		@RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
@@ -28,15 +30,42 @@ public class ProjectController {
 		@RequestParam(defaultValue = "id") String sortBy,
 		@RequestParam(defaultValue = "asc") String sortDirection
 	) {
-		PageResponse<ProjectDTO> response = projectService.getAll(
+		PageResponse<ProjectDTO> response = projectService.getProjects(
 			name, status, startDate, endDate, page, size, sortBy, sortDirection
 		);
 		return ResponseEntity.ok(response);
 	}
 	
 	@GetMapping("/{id}")
-	public Project getById(@PathVariable Long id) {
-		return projectService.getById(id);
+	public  ResponseEntity<ProjectDTO> getProjectById(
+		@PathVariable Long id
+	) {
+		ProjectDTO project = projectService.getProjectById(id);
+		return ResponseEntity.ok(project);
 	}
 	
+	@PostMapping
+	public ResponseEntity<ProjectDTO> createProject(
+		@RequestBody Project project
+	) {
+		ProjectDTO createdProject = projectService.createProject(project);
+		return ResponseEntity.status(HttpStatus.CREATED).body(createdProject);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<ProjectDTO> updateProject(
+		@PathVariable Long id,
+		@RequestBody Project project
+	) {
+		ProjectDTO updatedProject = projectService.updateProject(id, project);
+		return ResponseEntity.ok(updatedProject);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> deleteProject(
+		@PathVariable Long id
+	){
+		projectService.deleteProject(id);
+		return ResponseEntity.noContent().build();
+	}
 }
--- src/main/java/nelon/arrive/nelonshift/controller/ShiftController.java
+++ src/main/java/nelon/arrive/nelonshift/controller/ShiftController.java
@@ -2,15 +2,69 @@ package nelon.arrive.nelonshift.controller;
 
 
 import lombok.RequiredArgsConstructor;
+import nelon.arrive.nelonshift.dto.PageResponse;
+import nelon.arrive.nelonshift.dto.ShiftDTO;
+import nelon.arrive.nelonshift.entity.Shift;
 import nelon.arrive.nelonshift.service.ShiftService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
 
 @RestController
-@RequestMapping("${api.prefix}/projects/{projectId}/shifts")
+@RequestMapping("${api.prefix}/shifts")
 @RequiredArgsConstructor
 public class ShiftController {
 	private final ShiftService shiftService;
 	
+	@GetMapping
+	public ResponseEntity<PageResponse<ShiftDTO>> getShifts(
+		@RequestParam(required = false) Long projectId,
+		@RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
+		@RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate,
+		@RequestParam(required = false) Integer minHours,
+		@RequestParam(defaultValue = "0") int page,
+		@RequestParam(defaultValue = "10") int size,
+		@RequestParam(defaultValue = "date") String sortBy,
+		@RequestParam(defaultValue = "desc") String sortDirection
+		) {
+		PageResponse<ShiftDTO> response = shiftService.getShifts(
+			projectId, startDate, endDate, minHours, page, size, sortBy, sortDirection
+		);
+		return ResponseEntity.ok(response);
+	}
 	
+	@GetMapping("/{id}")
+	public ResponseEntity<ShiftDTO> getShiftById(
+		@PathVariable Long id
+	) {
+		ShiftDTO shift = shiftService.getShiftById(id);
+		return ResponseEntity.ok(shift);
+	}
+	
+	@PostMapping
+	public ResponseEntity<ShiftDTO> createShift(
+		@RequestParam Long projectId,
+		@RequestBody Shift shift
+		) {
+		ShiftDTO updatedShift = shiftService.createShift(projectId, shift);
+		return ResponseEntity.status(HttpStatus.CREATED).body(updatedShift);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<ShiftDTO> updateShift(
+		@PathVariable Long id,
+		@RequestBody Shift shift
+	) {
+		ShiftDTO updatedShift = shiftService.updateShift(id, shift);
+		return ResponseEntity.ok(updatedShift);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> deleteShift(@PathVariable Long id) {
+		shiftService.deleteShift(id);
+		return ResponseEntity.noContent().build();
+	}
 }
