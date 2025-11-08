a96595d91813ec5177d2b95af52fa4fc1fca8116
NelonArrive <nelon.arrive@gmail.com>
2025-11-09 01:54:33 +0500

    feat: update UserRepository to use UUID as the identifier

--- src/main/java/nelon/arrive/nelonshift/repositories/UserRepository.java
+++ src/main/java/nelon/arrive/nelonshift/repositories/UserRepository.java
@@ -2,6 +2,10 @@ package nelon.arrive.nelonshift.repositories;
 
 import nelon.arrive.nelonshift.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
 }
