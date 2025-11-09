e789f31f0875c0d3f82ffa446b4d216396af71eb
NelonArrive <nelon.arrive@gmail.com>
2025-11-09 23:10:40 +0500

    feat: add JWT utility class and authentication request/response DTOs

---
+++ src/main/java/nelon/arrive/nelonshift/dto/auth/MessageResponse.java
@@ -0,0 +1,12 @@
+package nelon.arrive.nelonshift.dto.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+class MessageResponse {
+	private String message;
+}
